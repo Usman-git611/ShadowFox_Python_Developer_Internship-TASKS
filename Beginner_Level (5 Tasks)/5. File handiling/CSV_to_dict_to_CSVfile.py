a822c86890ff88979e2f0c542996c9f873505f1d
Usman-git611 <usmankur611@gmail.com>
2026-08-16 19:59:33 +0530

    CSV to dictionary to CSV file(DONE)

---
+++ Beginner_Level (5 Tasks)/5. File handiling/CSV_to_dict_to_CSVfile.py
@@ -0,0 +1,36 @@
+import csv
+
+with open("student_marks.csv","r") as file:
+    reader=csv.DictReader(file)
+    students=[]
+
+    # Create dictionary
+    for row in reader:
+        student=dict(row)
+        marks = []
+        for key,value in student.items():
+            try:
+                marks.append(float(value))
+            except ValueError:
+                pass
+
+        total_marks = sum(marks)
+        average_marks = total_marks / len(marks)
+
+        student["total_marks"] = total_marks
+        student["Average"] = average_marks
+
+        students.append(student)
+print(students)
+for student in students:
+    print(student)
+
+
+# Create a new file
+with open("student_marks_result.csv", "w", newline="") as file:
+    fieldnames = students[0].keys()
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerows(students)
+
+print("\nNew file created successfully!")
