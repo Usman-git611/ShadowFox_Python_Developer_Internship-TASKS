8c4adfba58d691da250a1440b0c35f32ec6fdc94
Usman-git611 <usmankur611@gmail.com>
2026-08-16 15:09:57 +0530

    Simple Interest for 3 year (DONE)

---
+++ Beginner_Level (5 Tasks)/1.Variables/Simple_intersest.py
@@ -0,0 +1,7 @@
+principal_amount=1800
+rate_of_interest=9 # at 9%
+time=3 # for 3 years
+
+Simple_Interest= (principal_amount*rate_of_interest*time)/100; #Calculate the value
+print("Simple Interest = ",Simple_Interest) 
+#Display the output as :- Simple Interest =  486.0
